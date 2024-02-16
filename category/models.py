829db121f6362f94454943eb4c642c86f8133c02
Ivan Bondarenko <krenkroyt@gmail.com>
2024-02-16 20:59:09 +0400

    added: category admin config

--- category/models.py
+++ category/models.py
@@ -8,5 +8,9 @@ class Category(models.Model):
     description = models.TextField(max_length=255, blank=True)
     cat_image = models.ImageField(upload_to="photos/categories/", blank=True)
 
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
     def __str__(self):
         return self.category_name
