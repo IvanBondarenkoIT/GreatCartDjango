10dbace955128c80874688e662a17e9c38f25bfe
IvanBondarenkoIT <ivan.bondarenko.it@gmail.com>
2024-02-26 19:52:38 +0400

    added: Product model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+from category.models import Category
+
+from category.models import Category
+
+
 # Create your models here.
+class Product(models.Model):
+    product_name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField(max_length=500, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='photos/products', blank=True)
+    stock = models.IntegerField(default=0)
+    is_available = models.BooleanField(default=True)
+    categories = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.product_name
