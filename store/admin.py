bcef1048bc68b2e7d3a628e4ac1cbdcef49554a0
IvanBondarenkoIT <ivan.bondarenko.it@gmail.com>
2024-05-27 15:42:52 +0400

    added: ProductGallery

--- store/admin.py
+++ store/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
-from store.models import Product, Variations
+from store.models import Product, Variations, ProductGallery
+import admin_thumbnails
 
 
-# Register your models here.
+@admin_thumbnails.thumbnail('image')
+class ProductGalleryInline(admin.TabularInline):
+    model = ProductGallery
+    extra = 1
+
+
+@admin_thumbnails.thumbnail('image')
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_name', 'price', 'stock', 'category', 'modified_date', 'is_available')
     prepopulated_fields = {'slug': ('product_name',)}
+    inlines = [ProductGalleryInline]
 
 
 class VariationsAdmin(admin.ModelAdmin):
@@ -13,5 +21,7 @@ class VariationsAdmin(admin.ModelAdmin):
     list_editable = ('is_active', )
     list_filter = ('product', 'variation_category', 'variation_value')
 
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Variations, VariationsAdmin)
+admin.site.register(ProductGallery)
